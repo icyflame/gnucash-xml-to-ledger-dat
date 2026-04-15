d9964c4e6f3361af53ddb5449642daf1e9e19652
Siddharth Kannan <skannan@gitlab.com>
2026-04-15 18:53:35 +0530

    fix: Include the comments from conversion script in Golang

--- go/internal/writer/ledger.go
+++ go/internal/writer/ledger.go
@@ -42,20 +42,38 @@ func (w *LedgerWriter) Write(writer io.Writer) error {
 
 			accountName := w.parser.GetAccountName(split.Account)
 			commodity := account.Commodity.ID
-			quantity := parser.ParseFraction(split.Quantity)
-			value := parser.ParseFraction(split.Value)
 
+			// Bug: In a Stock purchase transaction, the "Edit Exchange Rate" dialog is not shown by
+			// GnuCash. This seems to be a limitation within the program. If the quantity for a
+			// transaction is zero, then it should attempt to use "split:value" instead. If that is zero
+			// too, then this split is a no-op and does not need to be included in the resulting Ledger
+			// file.  However, for correctness, I *will* include the "0 COMMODITY" split in the output
+			// Ledger file.
+			quantity := parser.ParseFraction(split.Quantity)
 			amount := quantity
+
+			value := parser.ParseFraction(split.Value)
+			if w.verbose {
+				fmt.Fprintf(os.Stderr, "%s, %g, %g\n", accountName, quantity, value)
+			}
 			if quantity == 0 && value != 0 {
 				amount = value
 				commodity = txn.Currency.ID
 			}
 
-			if w.verbose {
-				fmt.Fprintf(os.Stderr, "%s, %g, %g\n", accountName, quantity, value)
-			}
+			// Commodity names can have any character (including white space) if they are enclosed in double quotes.
+			//
+			// See section 4.5.1 Naming Commodities in the Ledger manual: https://ledger-cli.org/doc/ledger3.pdf
+			commodity = fmt.Sprintf("\"%s\"", commodity)
 
-			fmt.Fprintf(writer, "  %s  \"%s\"  %g\n", accountName, commodity, amount)
+			// TODO: For Stock purchases and sales, this should also have the price at which stocks were
+			// purchased, which will make it possible to use Ledger to calculate capital gains (maybe)
+			//
+			// See 4.5.2 Buying and Selling Stock in the Ledger manual: https://ledger-cli.org/doc/ledger3.pdf
+			fmt.Fprintf(writer, "  %s  %s  %g\n", accountName, commodity, amount)
+
+			// TODO: Include the second line from the transaction as a comment in the output Ledger file
+			// (The second line is visible when the Double line view is enabled in GnuCash)
 		}
 
 		fmt.Fprintln(writer)
@@ -79,3 +97,22 @@ func (w *LedgerWriter) formatDescription(description, txnID string) string {
 	}
 	return description
 }
+
+// Sample Ledger transaction:
+// 2015/10/12 Exxon
+//     Expenses:Auto:Gas         $10.00
+//     Liabilities:MasterCard   $-10.00
+//
+// Sample Stock purchase transaction:
+//
+// 2004/05/01 Stock purchase
+//   Assets:Broker                50 AAPL @ $30.00
+//   Expenses:Broker:Commissions  $19.95
+//   Assets:Broker                $-1,519.95
+//
+// The amount can be left out of the last posting:
+//
+// 2004/05/01 Stock purchase
+//   Assets:Broker                50 AAPL @ $30.00
+//   Expenses:Broker:Commissions  $19.95
+//   Assets:Broker
